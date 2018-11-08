3e95bd15f16dc72cbc29d0cd2e58c898277d1889
joel gamez <joelgamez14six@yahoo.com>
2018-11-08 13:13:50 -0500

    ReatRedux with multiple Reducers Example

--- src/App.js
+++ src/App.js
@@ -1,28 +1,79 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+
 import './App.css';
+import { connect } from  'react-redux';
 
 class App extends Component {
+
+
+  
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <div className="col">
+          <div> <span>A:</span><span>{this.props.a}</span></div>
+          <button onClick={() => this.props.updateA(this.props.b)}>Update A</button>         
+        </div>
+        <div className="col">
+          <div> <span>B:</span><span>{this.props.b}</span></div>
+          <button onClick={() => this.props.updateB(this.props.a)}>Update B</button>         
+        </div>
+
+
+
+        {/* <div className="age">Age: <span>{this.props.age}</span></div>
+        <button className="ageUp" onClick={this.props.onAgeUp}>Age UP</button>
+        <button className="ageDown" onClick={this.props.onAgeDown}>Age Down</button>
+        <hr></hr>
+     
+
+     
+        <div>History</div>
+        <div>
+          <ul>
+            {
+              this.props.history.map(el =>(
+                <li className="historyItem" key={el.id} onClick={() => this.props.onDeleteItem(el.id)}>
+                  {el.age}
+                </li>
+
+              ))
+            }
+          </ul>
+        </div> */}
       </div>
     );
   }
 }
 
-export default App;
+
+  const mapStateToProps = (state) => {
+    return {
+      a: state.rA.a,
+      b: state.rB.b
+
+    }
+
+  };
+
+  const mapDispatchToProps= (dispatch) => {
+    return {
+      updateA: (b) => dispatch({type:'UPDATE_A', b:b}),
+      updateB: (a) => dispatch({type:'UPDATE_B', a:a})
+    }
+  };
+// const mapStateToProps = (state) => {
+//   return {
+//     age: state.age,
+//     history: state.history
+//   }
+// }
+
+// const mapDispachToProps = (dispach) => {
+//   return {
+//     onAgeUp: () => dispach({type:'AGE_UP', value: 1}),
+//     onAgeDown: () => dispach({type:'AGE_DOWN', value: 1}),
+//     onDeleteItem: (id) => dispach({type:'DEL_ITEM', key: id})
+//   }
+
+export default connect(mapStateToProps,mapDispatchToProps)(App);
--- src/index.js
+++ src/index.js
@@ -4,7 +4,26 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import { Provider } from 'react-redux';
+import { createStore, combineReducers}  from 'redux';
+// import reducer from './store/reducer';
+import reducerA from './store/reducerA';
+import reducerB from './store/reducerB';
+
+const rootReducer = combineReducers({
+  rA: reducerA,
+  rB: reducerB 
+})
+
+
+const store = createStore(rootReducer);
+
+
+ReactDOM.render(
+                <Provider store={store}>
+                    <App />
+                </Provider>, 
+                document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
